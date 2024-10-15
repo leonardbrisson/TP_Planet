fb0ff91d6e3e2eedb4e4816a38c87d6386456bea
Leonard Brisson <leonard.brisson@imt-atlantique.net>
2024-10-15 12:13:47 +0200

    initial commit

---
+++ Assets/SolarSystemController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition(DateTime.Now);
+    }
+
+    void UpdatePosition(UDateTime t)
+    {
+            
+    }
+}
